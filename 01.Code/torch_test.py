8ed47ce5a57c229ab21b2f883f29c902cc34f3fb
hyr0771 <77557993+hyr0771@users.noreply.github.com>
2024-09-05 22:27:53 +0800

    Update torch_test.py

--- 01.Code/torch_test.py
+++ 01.Code/torch_test.py
@@ -1,10 +1,2 @@
 from loguru import logger
 import os
-
-logger.add('../logs/ffff.log')
-logger.info("hello, world!")
-logger.info("hello, world!")
-logger.info("hello, world!")
-logger.info("hello, world!")
-logger.info("hello, world!")
-logger.info("hello, world!")
